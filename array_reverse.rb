0e012ccb727c43342944943f835b0647bc03fb3a
sergejapetenok <exorich@yandex.ru>
2020-02-27 22:40:00 +0400

    Initial commit

---
+++ array_reverse.rb
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5]
+puts "Исходный массив:"
+puts numbers.to_s
+puts "Массив в обратном порядке:"
+puts numbers.reverse().to_s
+puts "Исходный массив (не изменился):"
+puts numbers.to_s
+puts "Исходный массив (после изменения):"
+puts numbers.reverse().to_s
